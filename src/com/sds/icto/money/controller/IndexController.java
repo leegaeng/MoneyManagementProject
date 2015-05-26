b111bf445159a001bfa75d1d6ecc0ca31d135451
이경화 <leegaeng>
2015-05-26 10:08:26 +0900

    index page

---
+++ src/com/sds/icto/money/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.sds.icto.money.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String index(){
+		System.out.println("¾È³ç????????");
+		return "main/index";
+	}
+	
+}
